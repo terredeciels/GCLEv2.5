c391013193da17b40122ec75b37093057f741fff
terredeciels <maildecontact@sfr.fr>
2012-11-02 13:01:04 +0100

    initial

---
+++ src/client/Client.java
@@ -0,0 +1,43 @@
+package client;
+
+import gposition.GPosition;
+import main.Fen;
+import main.Ui;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+//      String fen_initiale = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        GPosition position = GPosition.getInstance();
+
+//      position.init(fen_initiale);
+//      System.out.println(position);
+
+        //
+        String[] command = new String[3];
+
+        command[0] = "-cli";
+        command[1] = "F:/ProgmEchecsNotes/shirov.pgn";//814 parties
+//        command[1] = "F:/ProgmEchecsNotes/paulsen.pgn";//322
+//        command[1] = "F:/ProgmEchecsNotes/ashley.pgn";//414
+//          command[1] = "F:/ProgmEchecsNotes/bird.pgn";//353 
+//        command[1] = "F:/ProgmEchecsNotes/Tartakower.pgn";//1290
+//            command[1] = "F:/ProgmEchecsNotes/Capablanca.pgn";//597
+//        command[1] = "F:/ProgmEchecsNotes/Boleslavsky.pgn";//651
+//        command[1] = "F:/ProgmEchecsNotes/Soltis.pgn";//370
+//        command[1] = "F:/ProgmEchecsNotes/Motylev.pgn";//1169
+        Ui.main(command);
+
+//      ArrayList<String> fenliste = Fen.getFenList();
+        for (String f : Fen.getFenList()) {
+            position.init(f);
+            System.out.print(position);
+        }
+//   String fen = "8/1pR1P3/6b1/1k1p2p1/pP1r2P1/8/4KR2/8 w - - 1 57";
+//     
+//        position = GPosition.getInstance();
+//        position.init(fen);
+//        System.out.println(position);
+    }
+}
---
+++ src/gposition/generateur/IGcle.java
@@ -0,0 +1,17 @@
+package gposition.generateur;
+
+import java.util.*;
+
+public interface IGcle {
+
+    void diagonalePionAttaqueRoque(int caseO, int NordSudSelonCouleur, int estOuOuest);
+    void diagonalePionPrise(int caseO, int NordSudSelonCouleur, int estOuOuest);
+    ArrayList<GCoups> getPseudoCoups();
+    void getPseudoCoupsAttaque();
+    void pseudoCoups();
+    void pseudoCoupsDirection(int caseO, int direction);
+    void pseudoCoupsGlissant(int caseO, int[] directionsPiece);
+    void pseudoCoupsNonGlissant(int caseO, int[] directionsPiece);
+    void pseudoCoupsPion(int caseO, boolean recherchePionAttaqueRoque);
+    void pseudoCoupsPromotion(int caseO, int caseX, int pieceprise);
+}
